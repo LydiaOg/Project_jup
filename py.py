f1e30ec2132a8affc03e008c010179537639bba5
Lydia Edogiawerie <110990646+LydiaOg@users.noreply.github.com>
2022-11-11 14:43:58 +0000

    Create py.py

---
+++ py.py
@@ -0,0 +1,7 @@
+echo "# Python1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:LydiaOg/Python1.git
+git push -u origin main
